1a4cb0091a4339022013d18a0a64adba3b87973e
baboribo <inertia_mailer_0c@icloud.com>
2025-10-24 07:17:28 +0900

    ...

--- scripts/sync-content.ts
+++
@@ -1,9 +0,0 @@
-import algoliasearch from 'algoliasearch';
-import fs from 'fs';
-
-const client = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_ADMIN_KEY!);
-const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME!);
-
-const records = JSON.parse(fs.readFileSync('.next/server/app/static.json.body', 'utf-8'));
-await index.replaceAllObjects(records);
-console.log('✅ Uploaded successfully');
