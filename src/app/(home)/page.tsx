75f9d211cdbc6430c1e301ab9b3f6114644d3ebf
baboribo <inertia_mailer_0c@icloud.com>
2025-11-02 14:14:07 +0900

    로컬 문서 섹션을 'start'로 변경하고 관련 페이지를 추가 및 업데이트했습니다.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">세상이구나, 반가워</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        문서는 {' '}
         <Link
-          href="/docs"
+          href="/docs/start"
           className="text-fd-foreground font-semibold underline"
         >
-          /docs
+          /docs/local
         </Link>{' '}
-        and see the documentation.
+        에서 확인하실 수 있습니다.
       </p>
     </main>
   );
